d11b3ed455ec08f38f6fe413c5ea2eec5ed1e98b
vishal <vishal.yalameli@whitecode.in>
2024-11-21 09:54:32 +0530

    RunTime Polymorphism

---
+++ RuntimePolymorphism.java
@@ -0,0 +1,37 @@
+class Animal{
+     public void makeSound(){
+  System.out.println("make sound Public");
+}
+}
+ 
+class Dog extends Animal{
+
+  public void makeSound(){
+  System.out.println("Bark");
+}
+}
+
+class Cat extends Animal{
+  public void makeSound(){
+
+System.out.println("Maow");
+
+}
+}
+
+public class RuntimePolymorphism{
+
+ public static void main(String [] args){
+
+  Animal myAnimal;
+
+ myAnimal= new Dog();
+myAnimal.makeSound();
+
+myAnimal= new Cat();
+myAnimal.makeSound();
+}
+}
+
+
+ 
