4333c663a10bc08f8ae24ca4d88582cdbc30a4e4
vishal <vishal.yalameli@whitecode.in>
2024-11-21 12:04:29 +0530

    Compiletime Polymorphism

---
+++ ComplitimePolyporphism.java
@@ -0,0 +1,32 @@
+// compile time polymorphism
+
+class Calculator{
+  
+int add(int a , int b)
+{
+    return a + b;
+}
+
+int add(int a ,int c , int b){
+   return a + b;
+}
+
+double add( double a, double b){
+  return a + b;
+}
+}
+
+public class ComplitimePolyporphism{
+
+public static void main(String [] args)
+{
+   Calculator cal  = new Calculator();
+
+  System.out.println("sum of  10 and 20: " + cal.add(10,20));
+  System.out.println("sum of  15 and 20: " + cal.add(15, 10,20));
+System.out.println("Sum of 20.5 and 2.0: " + cal.add(20.5, 2.0));
+}
+}
+
+
+ 
