e58593bd48e33a559d25fc41c1a02704d9c4a841
vishal <yalamelivishal123@gmail.com>
2024-12-15 00:21:18 +0530

    Recursion

---
+++ RecursionEx.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursionEx{
+public static int Factorial(int n){
+
+if( n ==0 || n==1){
+return 1;
+}else{
+return n * Factorial(n-1);
+}
+
+}
+
+public static void main(String [] args){
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter the factorial number");
+int number = scanner.nextInt();
+
+int result = Factorial(number);
+System.out.print("Factorial number is:" + number + "is :" + result);
+scanner.close();
+}
+}
