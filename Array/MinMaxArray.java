824c7228624792abb954592301b11daaa0c6a4ef
vishal <yalamelivishal123@gmail.com>
2025-01-12 23:55:42 +0530

    int

---
+++ Array/MinMaxArray.java
@@ -0,0 +1,14 @@
+public class MinMaxArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 2, 8, -1, 4};
+        int min = arr[0], max = arr[0];
+        
+        for (int num : arr) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+        
+        System.out.println("Smallest: " + min);
+        System.out.println("Largest: " + max);
+    }
+}
