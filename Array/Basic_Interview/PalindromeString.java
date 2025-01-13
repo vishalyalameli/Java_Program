67b63af16f746070ddb7aa8091d28a853a2ac1a4
vishal <yalamelivishal123@gmail.com>
2025-01-14 00:43:29 +0530

    StringPalindrome

---
+++ Array/Basic_Interview/PalindromeString.java
@@ -0,0 +1,19 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
