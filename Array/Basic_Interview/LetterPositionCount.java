328339757741394422e968847ed740538f397105
vishal <yalamelivishal123@gmail.com>
2025-01-18 00:01:27 +0530

    GreatestOfTwo

--- Array/Basic_Interview/LetterPositionCount.java
+++ Array/Basic_Interview/LetterPositionCount.java
@@ -1,23 +1,20 @@
-public class LetterPositionCount
- {
- 
- public static void main(String args[])
- {
- 
- String s = "CANDIDJAVA";
- char[] a = s.toCharArray();
- int i = 1;
- {
- for (char output : a)
- {
- 
- System.out.print(output + " " + i + " ");
- i++;
- 
- }
- }
- 
- }
+public class LetterPositionCount {
+
+    public static void main(String args[]) {
+
+        String s = "CANDIDJAVA";
+        char[] a = s.toCharArray();
+        int i = 1;
+        {
+            for (char output : a) {
+
+                System.out.print(output + " " + i + " ");
+                i++;
+
+            }
+        }
+
+    }
 }
 
-// O/P:  C 1 A 2 N 3 D 4 I 5 D 6 J 7 A 8 V 9 A 10
+// O/P: C 1 A 2 N 3 D 4 I 5 D 6 J 7 A 8 V 9 A 10
