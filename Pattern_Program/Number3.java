e16a4424e50743db4646edae32b9331e7ed6f142
vishal <yalamelivishal123@gmail.com>
2025-01-27 23:58:40 +0530

    changes

---
+++ Pattern_Program/Number3.java
@@ -0,0 +1,35 @@
+class Number3{
+  public static void main(String [] args){
+   for(int i= 1; i<=4; i++){
+    for(int j= 1; j<=2*i; j++){
+       System.out.print("*");
+}
+System.out.println("");
+
+}
+
+}
+}
+
+/*
+ 
+12
+1234
+123456
+12345678
+
+
+ System.out.print(i);
+
+11
+2222
+333333
+44444444
+
+      System.out.print("*");
+**
+****
+******
+********
+
+*/
