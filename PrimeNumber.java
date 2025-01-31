b7312557d0de52d23c3b9546e29e3755c92cf4f4
vishal <yalamelivishal123@gmail.com>
2025-01-31 23:00:31 +0530

    prime number

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,25 +1,36 @@
- import java.util.Scanner;
-public class PrimeNumber{
-   public static void main(String[]args)
-   {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter Your Number");
-    int num  = sc.nextInt();
+import java.util.Scanner;
 
-  int count = 0;
-  
-for(int i = 1; i <= num; i++){
-  if(num%i== 0){
-  count ++ ;
-}  
-}
-  if(count == 2){
-   System.out.println("It is Prime Number");
-}else{
-   System.out.println("It is not PrimeNumber");
-  }
+public class PrimeNumber {
+    public static void main(String[] args) {
+                        Scanner sc = new Scanner(System.in); // Create Scanner object
+        System.out.print("Enter Your Number: ");
+        int num = sc.nextInt(); // Read user input
 
-sc.close();
+        int count = 0;
+
+        // Loop to count the number of divisors
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+count++;
+            }
+        }
+
+        // Check if the number is prime
+        if (count == 2) {
+            System.out.println(num + " is a Prime Number.");
+        } else {
+            System.out.println(num + " is NOT a Prime Number.");
+        }
+
+        sc.close(); // Close Scanner
+    }
 }
-}
- 
+
+
+// Enter Your Number
+// 5
+// It is Prime Number
+
+// Enter Your Number: 10
+// 10 is NOT a Prime Number.
+
