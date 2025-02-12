7523aeecd87f8eb44434a5a925b2e18fd6a74ac4
vishal <yalamelivishal123@gmail.com>
2025-02-12 23:00:44 +0530

    changes

--- Array/MinMaxArray.java
+++ Array/MinMaxArray.java
@@ -3,15 +3,15 @@ public class MinMaxArray {
         int[] arr = {3, 5, 7, 2, 8, -1, 4};
         int min = arr[0], max = arr[0];
         
-        for (int num : arr) {
-            if (num < min) min = num;
-            if (num > max) max = num;
-        }
+        // for (int num : arr) {
+        //     if (num < min) min = num;
+        //     if (num > max) max = num;
+        // }
         
         System.out.println("Smallest: " + min);
         System.out.println("Largest: " + max);
     }
 }
 
-// Smallest: -1
-// Largest: 8
+// Smallest: -1//
+// Largest: 8//
