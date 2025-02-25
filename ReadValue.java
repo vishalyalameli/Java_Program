fa96d07d35d36ba95dfdac811c9152e2643cda16
vishal <yalamelivishal123@gmail.com>
2025-02-25 23:07:16 +0530

    Program Read and Print an Integer value in Java

---
+++ ReadValue.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class ReadValue {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = s.nextInt();
+        
+        System.out.println("You entered: " + num);
+        
+        s.close();
+    }
+}
