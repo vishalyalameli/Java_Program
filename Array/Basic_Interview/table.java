16ffcaa5872c5775583a3c39f42317cd13d94816
vishal <yalamelivishal123@gmail.com>
2025-06-08 11:53:43 +0530

    Table Number Code

---
+++ Array/Basic_Interview/table.java
@@ -0,0 +1,8 @@
+class table{
+    public static void main(String [] args){
+        int num = 2;
+        for(int i = 1; i<=10; i++){
+            System.out.println(num + "*" + i+ "=" +(num * i));
+        }
+    }
+}
