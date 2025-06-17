e87af75f9017d5aa05acdd4f8ae71646e0f9ec2f
vishal <yalamelivishal123@gmail.com>
2025-06-17 23:29:04 +0530

    String Replace

--- Array/Interview/StringRepalce.java
+++ Array/Interview/StringRepalce.java
@@ -3,8 +3,25 @@ public class StringRepalce {
         String str = "My name is Vishal";
         String replcer = str.replace("Vishal", "vikas");
 
-        System.out.println("Original"+ str);
-        System.out.println("Repalce String" + replcer);
+        System.out.println("Original : "+ str);
+        System.out.println("Repalce String :" + replcer);
+    // OR
+        String str1 = "vishal";
+        String replacechar = str.replace('l', 'k');
+
+         System.out.println("Original : "+ str1);
+        System.out.println("Repalce String :" + replacechar);
+
+
     }
     
 }
+
+/*
+Original : My name is Vishal
+Repalce String :My name is vikas
+
+Original : vishal
+Repalce String :My name is Vishak
+
+ */
