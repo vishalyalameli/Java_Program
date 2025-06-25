7eeeefc5d8e8451a250cf257a9daceaf448c3c36
vishal <yalamelivishal123@gmail.com>
2025-06-25 22:38:49 +0530

    changes

--- Array/Interview/ArrayZero.java
+++ Array/Interview/ArrayZero.java
@@ -3,11 +3,11 @@
         int [] arr = { 1,0,5,0,4,0};
         int  n = arr.length;
 
-        for(int i = 0 ; i<n ; i++){
-            if(arr[i] != 0){
-                System.out.println(arr[i] + " ");
-            }
-        }
+        // for(int i = 0 ; i<n ; i++){
+        //     if(arr[i] != 0){
+        //         System.out.println(arr[i] + " ");
+        //     }
+        // }
 
         for(int i = 0; i<n ; i++){
             if(arr[i] == 0){
