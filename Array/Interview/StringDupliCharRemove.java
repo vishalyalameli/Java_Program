b86de5ca1132da57f0e4a737954f63d67cf05de6
vishal <yalamelivishal123@gmail.com>
2025-07-02 17:51:25 +0530

    String Remove Duplicate Element

---
+++ Array/Interview/StringDupliCharRemove.java
@@ -0,0 +1,19 @@
+class StringDupliCharRemove {
+    public static void main(String [] args){
+        String input = "geeksforgeeks";
+        String output= "";
+
+        for(int i = 0 ;i< input.length() ; i++) {
+
+            char CurrentChar = input.charAt(i);
+
+            if(output.indexOf(CurrentChar) == -1 ){
+                output = output + CurrentChar;
+            }
+
+        }
+        System.out.println("Output:" + output);
+    }
+
+    
+}
