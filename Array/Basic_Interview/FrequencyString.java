e79d5335253ecc87f4b1b781e076f4c41a726fc8
vishal <yalamelivishal123@gmail.com>
2025-07-21 18:10:16 +0530

    Frequency String

---
+++ Array/Basic_Interview/FrequencyString.java
@@ -0,0 +1,38 @@
+ class FrequencyString {
+    public static void main(String[] args){
+        String str = "banana";
+
+        for(int i =0 ; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            boolean alredaycount = false;
+
+            for(int j = 0; j<i ; j++){
+                if(str.charAt(j) ==ch){
+                    alredaycount =true;
+                    break;
+
+                }
+
+            }
+
+            if(alredaycount){
+                continue;
+            }
+
+            int count = 0;
+
+            for(int k =0 ; k<str.length(); k++){
+                if(str.charAt(k)==ch){
+                    count++;
+                }
+            }
+
+            System.out.println(ch +"="+ count);
+
+
+        }
+
+    }
+    
+}
