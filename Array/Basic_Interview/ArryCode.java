fe70ce2c153dfab056d99bcb9dbb1a609f96d519
vishal <yalamelivishal123@gmail.com>
2025-07-23 23:41:28 +0530

    Array

--- Array/Basic_Interview/ArryCode.java
+++ Array/Basic_Interview/ArryCode.java
@@ -3,6 +3,7 @@ class ArryCode {
 
     public static void main(String [] args ){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of Array");
         int size = sc.nextInt();
         int arr [] = new int[size];
         int product = 1;
@@ -15,7 +16,7 @@ class ArryCode {
         System.out.println("Array");
 
         for(int i=0; i<arr.length;i++){
-            product = product/arr[i];
+           arr[i]=product/arr[i];
             System.out.println(arr[i]);
         }
         sc.close();
