ecbfafea906746e1dfbef47704cecb142278acfa
vishal <yalamelivishal123@gmail.com>
2025-08-21 23:48:11 +0530

    Array

--- Array/Basic_Interview/ArrayOddEvenNum.java
+++ Array/Basic_Interview/ArrayOddEvenNum.java
@@ -2,7 +2,7 @@
 
 public class ArrayOddEvenNum {
     public static void main(String [] args){
-        int [] array = {1,2,3,4,5,6,7,8,9,10};
+        int [] array = {1,2,3,4,5,6,7,8,9,10,11};
         int []  count = countOddAndEven(array);
 
         System.out.println("Even number :" + count [1]);
