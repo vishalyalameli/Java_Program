ac01717c19d2784a2cf73dc9dca9742f16e0db84
vishal <yalamelivishal123@gmail.com>
2025-08-22 11:24:53 +0530

    Array code Practice

--- Array/Basic_Interview/ArrayOddEvenNum.java
+++ Array/Basic_Interview/ArrayOddEvenNum.java
@@ -1,32 +1,61 @@
 
-
 public class ArrayOddEvenNum {
-    public static void main(String [] args){
-        int [] array = {1,2,3,4,5,6,7,8,9,10,11};
-        int []  count = countOddAndEven(array);
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        int[] count = countOddAndEven(array);
 
-        System.out.println("Even number :" + count [1]);
-        System.out.println("Odd number :" + count [0]);
+        System.out.println("Even number :" + count[1]);
+        System.out.println("Odd number :" + count[0]);
     }
-    public static int[] countOddAndEven(int [] array){
-        int [] count = new int[2];
-        for(int num :array){
-            if(num % 2 == 0){
+
+    public static int[] countOddAndEven(int[] array) {
+        int[] count = new int[2];
+        for (int num : array) {
+            if (num % 2 == 0) {
                 count[1]++;
 
-            }else{
+            } else {
                 count[0]++;
             }
         }
         return count;
 
     }
-    
+
 }
 
 /*
-Even number :5
-Odd number :5
+ * Even number :5
+ * Odd number :5
  */
 
- 
+class EvenAndOdd {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        int even_count = 0;
+        int odd_count = 0;
+
+        System.out.println("Odd Number : ");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+
+                System.out.println(array[i] + " ");
+                odd_count++;
+
+            }
+        }
+
+        System.out.println("Odd Number:" + odd_count);
+
+        System.out.println(" Even Number : ");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i] + " ");
+                even_count++;
+
+            }
+        }
+        System.out.println(" Even Number:" + even_count);
+
+    }
+}
